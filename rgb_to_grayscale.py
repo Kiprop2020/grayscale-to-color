253fd68aa0f47d5134aa9e059d883ddaae9dd0f3
Peter Kiprop <71701881+Kiprop2020@users.noreply.github.com>
2022-12-20 13:26:38 +0300

    Update rgb_to_grayscale.py

--- rgb_to_grayscale.py
+++ rgb_to_grayscale.py
@@ -2,31 +2,27 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 from io import BytesIO
-import numpy 
 from tensorflow.image import rgb_to_grayscale
 from tensorflow.keras.utils import array_to_img, img_to_array
 
-def convert_image(image):
+def save_image(image):
   buf = BytesIO()
   image.save(buf, format='png')
   byte_im = buf.getvalue()
   return byte_im
 
 def convert_image(upload):
-  with open(upload, 'rb') as f:
-    image_bytes = f.read()
-    image = tf.io.decode_png(image_bytes)
-    
+  image = Image.open(upload)
+  rgb_image = image.convert("RGB")
   col1.write("Original Image :camera")
-  col1.image(image)
+  col1.image(rgb_image)
 
-  converted = rgb_to_grayscale(image)
-#   converted = tf.keras.utils.img_to_array(converted)
-  converted = array_to_img(converted)
-  col2.write("Fixed Image :wrench:")
-  col2.image(converted)
+  gray_image = array_to_img(rgb_to_grayscale(rgb_image))
+  col2.write("Converted Image :wrench:")
+  col2.image(gray_image)
+  
   st.sidebar.markdown("\n")
-  st.sidebar.download_button("Download converted image", convert_image(converted), "converted.png", "image/png")
+  st.sidebar.download_button("Download converted image", save_image(gray_image), "gray_image.png", "image/png")
   
   
 col1, col2 = st.columns(2)
