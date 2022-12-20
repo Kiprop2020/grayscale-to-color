80fb77acf2c46a788db16751d9ee2512b9356fe4
Peter Kiprop <71701881+Kiprop2020@users.noreply.github.com>
2022-12-20 07:41:46 +0300

    Add files via upload

---
+++ rgb_to_grayscale.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from PIL import Image
+
+input_source = "C:/Users/negehiza/OneDrive - NACC/Pictures/profile.webp"
+output_source = "C:/Users/negehiza/OneDrive - NACC/Pictures/out_profile.jpg"
+
+image = Image.open(input_source)
+image = tf.image.rgb_to_grayscale(image)
+image = tf.keras.utils.img_to_array(image)
+image = tf.keras.utils.array_to_img(image)
+image.save(output_source, format=None)
+Image.open(output_source)
